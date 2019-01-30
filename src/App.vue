304495319ac788a0e607cb5735249a2248ad112c
王嘉巍 <906789088@qq.com>
2019-01-31 01:51:44 +0800

    feat: using flex

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
-  <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <router-view/>
+  <div class="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      I am content!
+    </div>
   </div>
 </template>
 
 <script>
+import header from './components/header/header.vue'
 export default {
-  name: 'App'
+  components: {
+    'v-header': header
+  }
 }
 </script>
 
-<style>
-
+<style lang="stylus">
+  .app
+    .tab
+      display: flex
+      width: 100%
+      height: 40px;
+      line-height: 40px;
+      .tab-item
+        flex: 1
+        text-align: center
 </style>
