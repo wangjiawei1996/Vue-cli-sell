9040b69914e6664f475da9b21609db155ea664a0
王嘉巍 <906789088@qq.com>
2019-02-08 11:08:04 +0800

    feat: add Vue-resource function

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import goods from './components/goods/goods.vue'
 import ratings from './components/ratings/ratings.vue'
 import seller from './components/seller/seller.vue'
 import './common/stylus/index.styl'
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
