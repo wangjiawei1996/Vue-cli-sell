2f9890da4b50055c061fa9401defcd22ad24ea17
王嘉巍 <906789088@qq.com>
2019-02-11 12:40:42 +0800

    feat: add png to vue

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -42,10 +42,12 @@ export default {
       font-size: 0
       .avatar
         display: inline-block
+        vertical-align: top
+        img
+          border-radius: 2px
       .content
         display: inline-block
         margin-left: 16px
-        font-size: 14px
         .title
           margin: 2px 0 8px 0
           .brand
@@ -61,4 +63,26 @@ export default {
             font-size: 16px
             line-height: 18px
             font-size: bold
+        .description
+          margin-bottom: 10px
+          line-height: 12px
+          font-size: 12px
+        .support
+          .icon
+            display: inline-block
+            width: 12px
+            height: 12px
+            margin-right: 4px
+            background-size: 12px 12px
+            background-repeat: no-repeat
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
 </style>
