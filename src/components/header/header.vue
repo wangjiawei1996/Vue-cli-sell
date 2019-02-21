8b90a2145dabfc270153e8bdb5b1720f9754717f
王嘉巍 <906789088@qq.com>
2019-02-22 01:19:28 +0800

    finish: header.vue's built

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -26,38 +26,40 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%" />
     </div>
-    <div v-show="detailShow" class="detail">
-      <div class="detail-wrapper claerfix">
-        <div class="detail-main">
-          <h1 class="name">{{seller.name}}</h1>
-          <div class="star-wrapper">
-            <star :size="48" :score="seller.score"></star>
-          </div>
-          <div class="title">
-            <div class="line"></div>
-            <div class="text">优惠信息</div>
-            <div class="line"></div>
-          </div>
-          <ul v-if="seller.supports" class="supports">
-            <li class="support-item" v-for="(item, index) in seller.supports" :key="index">
-              <span class="icon" :class="classMap[seller.supports[index].type]"></span>
-              <span class="text">{{seller.supports[index].description}}</span>
-            </li>
-          </ul>
-          <div class="title">
-            <div class="line"></div>
-            <div class="text">商家公告</div>
-            <div class="line"></div>
-          </div>
-          <div class="bulletin">
-            <p class="content">{{seller.bulletin}}</p>
+    <transition name="fade">
+      <div v-show="detailShow" class="detail">
+        <div class="detail-wrapper claerfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="(item, index) in seller.supports" :key="index">
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
           </div>
         </div>
+        <div class="detail-close" @click="hideDetail">
+          <img class="icon-close" src='./close.png' />
+        </div>
       </div>
-      <div class="detail-close">
-        <img class="icon-close" src='./close.png' />
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -77,6 +79,9 @@ export default {
   methods: {
     showDetail() {
       this.detailShow = true
+    },
+    hideDetail() {
+      this.detailShow = false
     }
   },
   created() {
@@ -209,7 +214,14 @@ export default {
       left: 0
       top: 0
       overflow: auto
+      backdrop-filter: blur(10px)
+      opacity: 1
       background: rgba(7, 17, 27, 0.8)
+      &.fade-enter-active, &.fade-leave-active
+        transition: all 0.5s
+      &.fade-enter, &.fade-leave
+        opacity: 0
+        background: rgba(7, 17, 27, 0)
       .detail-wrapper
         width: 100%
         min-height: 100%
@@ -266,10 +278,15 @@ export default {
                 &.special
                   bg-image('special_2')
               .text
-                line-height: 12px
+                line-height: 16px
                 font-size: 12px
           .bulletin
-            
+            width: 80%
+            margin: 0 auto
+            .content
+              padding: 0 12px
+              line-height: 24px
+              font-size: 12px
       .detail-close
         position: relative
         margin: -64px auto 0 auto
