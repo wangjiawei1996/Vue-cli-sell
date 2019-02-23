15a4937896e51c8e145b228dbfa40c52ec356cf8
王嘉巍 <906789088@qq.com>
2019-02-23 17:43:42 +0800

    feat: finish menu-wrapper flex:table

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper"></div>
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="(item, index) in goods" :key="index" class="menu-item">
+          <span class="text border-1px">
+            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
     <div class="foods-wrapper"></div>
   </div>
 </template>
@@ -20,6 +28,7 @@ export default {
     }
   },
   created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
     this.$http.get('api/goods').then(response => {
       response = response.body
       if (response.errno === ERR_OK) {
@@ -42,6 +51,36 @@ export default {
     .menu-wrapper
       flex: 0 0 80px
       background: #f3f5f7
+      .menu-item
+        display: table
+        height: 54px
+        width: 56px
+        padding: 0 12px
+        line-height: 14px
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 12px
+          height: 12px
+          margin-right: 2px
+          background-size: 12px 12px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+        .text
+          display: table-cell
+          width: 56px
+          vertical-align: middle
+          border-1px(rgba(7, 17, 27, 0.1))
+          font-size: 12px
     .foods-wrapper
       flex: 1
 </style>
