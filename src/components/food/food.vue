df74bcfdac87736b66298a9d8d199400e7cbee83
王嘉巍 <906789088@qq.com>
2019-03-10 20:55:43 +0800

    feat:add food.vue in good

---
+++ src/components/food/food.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="food" v-show="showFlag"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    food: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~common/stylus/minin'
+  .food
+    position: fixed
+    left: 0
+    top: 0
+    bottom: 48px
+    z-index: 30
+    width: 100%
+    background: #fff
+</style>
