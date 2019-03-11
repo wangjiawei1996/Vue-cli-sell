05cc42c265202f289fc61edfab96629ca4e8d7be
王嘉巍 <906789088@qq.com>
2019-03-11 11:46:09 +0800

    feat: using icon-arrow_lift display showFlag = false

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -4,6 +4,20 @@
       <div class="food-content">
         <div class="image-header">
           <img :src="food.image" />
+          <div class="back" @click="hide">
+            <img src="./icon-arrow_lift.png" class="icon-arrow_lift" />
+          </div>
+        </div>
+        <div class="content">
+          <h1 class="content">{{food.name}}</h1>
+          <div class="detail">
+            <span class="sell-count">月售{{food.sellCount}}份</span>
+            <span class="rating">好评率{{food.rating}}</span>
+          </div>
+          <div class="price">
+            <span class="now">¥{{food.price}}</span>
+             <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+          </div>
         </div>
       </div>
     </div>
@@ -25,6 +39,9 @@ export default {
   methods: {
     show() {
       this.showFlag = true
+    },
+    hide() {
+      this.showFlag = false
     }
   }
 }
@@ -44,4 +61,25 @@ export default {
     transform: translate3d(0, 0, 0)
     &.move-enter, &.move-leave-to
       transform: translate3d(100%, 0, 0)
+    .food-content
+      .image-header
+        position: relative
+        width: 100%
+        height: 0
+        padding-top: 100%
+        img
+          position: absolute
+          top: 0
+          left: 0
+          width: 100%
+          height: 100%
+        .back
+          position: absolute
+          top: 10px
+          left: 0
+          .icon-arrow_lift
+            display: block
+            padding: 10px
+            width: 20px
+            height: 20px
 </style>
