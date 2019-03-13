76ca5767cc6b76ee61dec8c4ba86f81e4ad996c1
王嘉巍 <906789088@qq.com>
2019-03-13 22:07:08 +0800

    feat: module add positive negative

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -33,7 +33,7 @@
         <split></split>
         <div class="rating">
           <h1 class="title">商品评价</h1>
-          <ratingselect></ratingselect>
+          <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
         </div>
       </div>
     </div>
@@ -45,6 +45,9 @@ import BSroll from 'better-scroll'
 import cartcontrol from '../cartcontrol/cartcontrol'
 import ratingselect from '../ratingselect/ratingselect'
 import split from '../split/split'
+  // const POSITIVE = 0
+  // const NEGATIVE = 1
+  const ALL = 2
 export default {
   props: {
     food: {
@@ -53,12 +56,21 @@ export default {
   },
   data() {
     return {
-      showFlag: false
+      showFlag: false,
+      selectType: ALL,
+      onlyContent: true,
+      desc: {
+        all: '全部',
+        positive: '推荐',
+        negative: '吐槽'
+      }
     }
   },
   methods: {
     show() {
       this.showFlag = true
+      this.selectType = ALL
+      this.onlyContent = true
       this.$nextTick(() => {
         if (!this.scroll) {
           this.scroll = new BSroll(this.$refs.food, {
@@ -190,4 +202,11 @@ export default {
           padding: 0 8px
           font-size: 12px
           color: rgb(77, 85, 93)
+      .rating
+        padding-top: 18px
+        .title
+          line-height: 14px
+          margin-left: 18px
+          font-size: 14px
+          color: rgb(7, 17, 27)
 </style>
--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="ratingselect">
-    <div class="rating-type">
+    <div class="rating-type border-1px">
       <span>{{desc.all}}</span>
       <span>{{desc.positive}}</span>
       <span>{{desc.negative}}</span>
     </div>
     <div class="switch">
-      <span></span>
+      <span>
+        <img src="./select.png" />
+      </span>
       <span class="text">只看有内容的评价</span>
     </div>
   </div>
@@ -47,5 +49,10 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-
+  @import '~common/stylus/minin'
+  .ratingselect
+    .rating-type
+      padding: 18px 0
+      margin: 0 18px
+      border-1px(rgba(7, 17, 27, 0.1))
 </style>
