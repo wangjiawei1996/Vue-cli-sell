60c739be9fe793eb835a54b9672c5a1418c5e07f
王嘉巍 <906789088@qq.com>
2019-03-15 18:09:23 +0800

    feat: ratingselecr computed negative.length

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -2,8 +2,8 @@
   <div class="ratingselect">
     <div class="rating-type border-1px">
       <span @click="select(2,$event)" class="block positive" :class="{'active':selectType===2}">{{desc.all}}<span class="count">{{ratings.length}}</span></span>
-      <span @click="select(0,$event)" class="block positive" :class="{'active':selectType===0}">{{desc.positive}}<span class="count">40</span></span>
-      <span @click="select(1,$event)" class="block negative" :class="{'active':selectType===1}">{{desc.negative}}<span class="count">7</span></span>
+      <span @click="select(0,$event)" class="block positive" :class="{'active':selectType===0}">{{desc.positive}}<span class="count">{{positive.length}}</span></span>
+      <span @click="select(1,$event)" class="block negative" :class="{'active':selectType===1}">{{desc.negative}}<span class="count">{{ratings.length - positive.length}}</span></span>
     </div>
     <div class="switch" @click="Only">
       <span>
@@ -15,7 +15,7 @@
 </template>
 
 <script>
-  // const POSITIVE = 0
+  const POSITIVE = 0
   // const NEGATIVE = 1
   const ALL = 2
 export default {
@@ -43,6 +43,13 @@ export default {
           negative: '不满意'
         }
       }
+    }
+  },
+  computed: {
+    positive () {
+      return this.ratings.filter((rating) => {
+        return rating.rateType === POSITIVE
+      })
     }
   },
   methods: {
