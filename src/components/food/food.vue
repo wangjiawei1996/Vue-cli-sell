b936a4e1dfb43e06736ed34470630ff0a6e1aff1
王嘉巍 <906789088@qq.com>
2019-03-16 23:55:56 +0800

    feat: add rating-wrapper in food.vue

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -34,6 +34,21 @@
         <div class="rating">
           <h1 class="title">商品评价</h1>
           <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings" @select="selected" @only="only"></ratingselect>
+          <div class="rating-wrapper">
+            <ul v-show="food.ratings && food.ratings.length">
+              <li v-for="rating in food.ratings" class="rating-item" :key="rating.index">
+                <div class="user">
+                  <span class="name">{{rating.username}}}</span>
+                  <img class="avatar" width="12" height="12" :src="rating.avatar" />
+                </div>
+                <div class="time">{{rating.rateTime}}</div>
+                <p class="text">
+                  <span :class="{'icon-thumb_up' : rating.rateType === 0, 'icon-thumb_down' : rating.rateType === 1}"></span>{{rating.text}}
+                </p>
+              </li>
+            </ul>
+            <div class="no-rating" v-show="!food.ratings || !food.ratings.length"></div>
+          </div>
         </div>
       </div>
     </div>
